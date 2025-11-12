328d70b25823ad1e6cf2d8331915838e882ed79e
Kaique Sacchi <kaique.sacchi@flashapp.com.br>
2025-11-11 23:51:52 -0300

    removed boilerplate

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,5 +1,3 @@
-import "./assets/main.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -7,5 +5,5 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
